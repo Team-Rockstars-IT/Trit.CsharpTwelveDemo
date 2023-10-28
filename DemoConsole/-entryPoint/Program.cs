e6983b712f6b16020776e8f66e9a97a9717e06f1
Vincent Spaa <vincent.spaa@teamrockstars.nl>
2023-10-28 15:04:36 +0200

    Initial commit with Primary Constructors

---
+++ DemoConsole/-entryPoint/Program.cs
@@ -0,0 +1,47 @@
+using DemoConsole = Trit.DemoConsole;
+
+while (true)
+{
+    Clear();
+    WriteLine("Pick a hex number: ");
+    var picked = ReadKey().KeyChar;
+    Clear();
+
+    /*
+     * Language features:
+     * ref readonly parameters as a replacement for `in` parameters, to disallow rvalue
+     * collection expressions (i.e. var a = [] {}), in combination with a custom collection builder?
+     * interceptors
+     * inline arrays
+     * nameof with instance members
+     * alias for any type
+     * Primary Constructors
+     * Lambda optional parameters
+     *
+     * Other features to consider:
+     * Private field access via attributes
+     * System.Collections.Frozen
+     * TimeProvider
+     * Tensor primitives
+     * System.Text.Json improvements: GetFromJsonAsAsyncEnumerable, JsonNode.ParseAsync, JsonSourceGenerationOptionsAttribute, JsonSerializer.Serialize<Memory<int>>, [JsonConverter(typeof(JsonStringEnumConverter<MyEnum>))], JsonSerializer.IsReflectionEnabledByDefault
+     *      [JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Disallow)]
+     * ZipFile.CreateFromDirectory ?
+     * ASP.NET Core being prepared for AOT: [OptionsValidator] AddSingleton<IValidateOptions<FirstModelNoNamespace>, FirstValidatorNoNamespace>()
+     * SHA3_256?
+     * Build to container as .tar.gz?
+     * AddMetrics() with Meter
+     * dotnet build /tl
+     * <NuGetAuditLevel>moderate</NuGetAuditLevel>
+     * Vector512
+     */
+
+    // All sources available
+    // @ https://github.com/Team-Rockstars-IT/Trit.CsharpTwelveDemo
+    await (char.ToUpperInvariant(picked) switch
+    {
+        '1' => DemoConsole._1_PrimaryConstructors.Demo.Main(),
+        _ => Task.CompletedTask
+    });
+
+    ReadLine();
+}
---
+++ DemoConsole/1-PrimaryConstructors/Program.cs
@@ -0,0 +1,79 @@
+﻿// ReSharper disable All
+namespace Trit.DemoConsole._1_PrimaryConstructors;
+
+public static class Demo
+{
+    public static Task Main()
+    {
+        var sandwich = new Sandwich(Ingredient.Ham, Ingredient.Jelly, BreadType.WholeGrain);
+
+        WriteLine($"{sandwich}, {(sandwich.IsWholeGrain() ? "It's whole-grain!" : "It's not")}");
+
+        return Task.CompletedTask;
+    }
+
+    #region Before
+
+    // BEFORE FEATURE: Primary Constructors
+    public class OldSandwich : Food
+    {
+        private readonly Ingredient? _secondIngredient;
+        private readonly BreadType _breadType;
+
+        [WillBeAppliedToTheConstructor]
+        public OldSandwich(Ingredient firstIngredient, Ingredient? secondIngredient, BreadType breadType)
+            : base(nameof(OldSandwich))
+        {
+            FirstIngredient = firstIngredient;
+            _secondIngredient = secondIngredient;
+            _breadType = breadType;
+        }
+
+        public Ingredient FirstIngredient { get; }
+
+        public Ingredient? SecondIngredient => _secondIngredient;
+
+        public bool IsWholeGrain() => _breadType == BreadType.WholeGrain;
+
+        public override string ToString() =>
+            $"{_breadType} bread with {FirstIngredient}{(_secondIngredient.HasValue ? $"and {_secondIngredient}" : "")}";
+    }
+
+    #endregion
+
+    #region After
+
+    [method: WillBeAppliedToTheConstructor]
+    // FEATURE: Primary Constructors
+    public class Sandwich(Ingredient firstIngredient, Ingredient? secondIngredient, BreadType breadType)
+        : Food(name: nameof(Sandwich))
+    {
+        public Sandwich() : this(Ingredient.PeanutButter, Ingredient.Jelly, BreadType.Sourdough) { }
+
+        // The compiler hints to use this property in ToString,
+        // because with how it's written now: two private backing fields are created!
+        public Ingredient FirstIngredient { get; } = firstIngredient;
+
+        public Ingredient? SecondIngredient => secondIngredient;
+
+        public bool IsWholeGrain() => breadType == BreadType.WholeGrain;
+
+        public override string ToString() =>
+            $"{breadType} bread with {firstIngredient}{(secondIngredient.HasValue ? $"and {secondIngredient}" : "")}";
+    }
+
+    #endregion
+
+    #region Support
+
+    public enum Ingredient { PeanutButter, Jelly, Ham, Cheese }
+
+    public enum BreadType { WholeGrain, Sourdough, Rye }
+
+    public class Food(string name);
+
+    [AttributeUsage(AttributeTargets.Constructor)]
+    public sealed class WillBeAppliedToTheConstructorAttribute : Attribute;
+
+    #endregion
+}
